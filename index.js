1e3bd6afcd83b823a74db621cb918aa5eeb9ddb6
tunde Oretade <tundeiness@yahoo.co.uk>
2017-12-30 22:41:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
   if(string !== string.toLowerCase){
-    return 
+    return "YES INDEED!"
   }else if(string === "I love you, Grandma." ){
     return "I love you, too."
   }else{
-    return "YES INDEED!"
+    return 
   }  "I can\'t hear you!"
 }

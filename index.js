0adf599f8dd0696983592ec5e314d4ab3ee34569
tunde Oretade <tundeiness@yahoo.co.uk>
2017-12-30 22:57:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,10 @@ function sayHiToGrandma(string){
     return "I can\'t hear you!"
   }
   
+  if(string === string.toUpperCase()){
+    return "YES INDEED"
+  }
+  
   if(string === "I love you, Grandma." ){
     return "I love you, too."
   }

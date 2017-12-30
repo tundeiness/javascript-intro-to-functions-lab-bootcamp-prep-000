46fb1a3d11a89fb19510a9ea2e00b87cf499d538
tunde Oretade <tundeiness@yahoo.co.uk>
2017-12-30 22:42:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,6 @@ function sayHiToGrandma(string){
   }else if(string === "I love you, Grandma." ){
     return "I love you, too."
   }else{
-    return 
-  }  "I can\'t hear you!"
+    return "I can\'t hear you!"
+  }  
 }
